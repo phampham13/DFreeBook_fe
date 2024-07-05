b7520768a4613e3fe0a1d2e5ba9755af96942e0b
phampham13 <phamthikimngan.1013@gmail.com>
2024-07-06 01:17:50 +0700

    fix reload admin 3

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,8 @@
-
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import EnvironmentPlugin from "vite-plugin-environment";
 
-// export default defineConfig(({ mode }) => {
-
-
-//   return {
-//     plugins: [
-//       react(),
-//       EnvironmentPlugin([
-//         "REACT_API_URL_BACKEND",
-//         "VITE_API_KEY",
-//         "VITE_AUTH_DOMAIN",
-//         "VITE_PROJECT_ID",
-//         "VITE_STORAGE_BUCKET",
-//         "VITE_MESSAGING_SENDER_ID",
-//         "VITE_APP_ID",
-//         "VITE_MEASUREMENT_ID",
-//       ]),
-//     ],
-//     define: {
-//       'process.env': process.env
-//     }
-//   };
-// });
-
-// ...
 export default defineConfig({
-  // ...
   plugins: [
     react(),
     EnvironmentPlugin([
@@ -46,5 +19,17 @@ export default defineConfig({
   ],
   define: {
     'process.env': process.env
+  },
+  server: {
+    historyApiFallback: true
+  },
+  build: {
+    rollupOptions: {
+      input: '/index.html',
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    base: './',
   }
-})
+});
+
