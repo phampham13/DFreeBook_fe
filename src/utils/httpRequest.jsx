1729608712ec776355da0e485d5d8380f1eb5681
phampham13 <phamthikimngan.1013@gmail.com>
2024-07-16 17:08:48 +0700

    fix env

--- src/utils/httpRequest.jsx
+++ src/utils/httpRequest.jsx
@@ -1,11 +1,20 @@
 import axios from "axios";
+let api = "";
+console.log("VITE", import.meta.env);
+console.log("Process", process.env);
 
+if (import.meta.env.MODE === "dev") {
+    api = `${import.meta.env.VITE_API_LOCAL}`;
+} else {
+    api = `${import.meta.env.VITE_API_BACKEND}`;
+}
 export const req = axios.create({
-    baseURL: `${process.env.REACT_API_URL_BACKEND}`,
+    baseURL: api,
     withCredentials: true,
 });
 
 export const axiosJWT = axios.create({
-    baseURL: `${process.env.REACT_API_URL_BACKEND}`,
+    baseURL: api,
     withCredentials: true,
-})
+});
+console.log("API", api);
